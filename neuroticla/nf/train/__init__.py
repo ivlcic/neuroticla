50005ffb8103f8c3aeb448bb8116b59bcadec8a5
Nikola Ivačič <nikola.ivacic@dropchop.com>
2023-08-24 08:28:08 +0200

    Porting Newsframes classifiers code.

--- neuroticla/nf/train/__init__.py
+++ neuroticla/nf/train/__init__.py
@@ -3,12 +3,12 @@ import logging
 
 from transformers import TrainingArguments
 
-from neuroticla.core.dataset import TokenClassifyDataset
+from neuroticla.core.dataset import SeqClassifyDataset
 from neuroticla.core.json import NpEncoder
-from neuroticla.core.labels import Labeler
+from neuroticla.core.labels import Labeler, BinaryLabeler
 from neuroticla.core.split import DataSplit
 from neuroticla.core.trans import TokenClassifyModel, ModelContainer
-from neuroticla.ner.utils import *
+from neuroticla.nf.utils import *
 
 logger = logging.getLogger('nf.train')
 
@@ -19,15 +19,64 @@ def add_args(nrcla_module: str, parser: ArgumentParser) -> None:
     parser.add_argument(
         '-n', '--model_name', help='Target model name.', type=str, default=None
     )
+    labels = ','.join(get_all_labels(nrcla_module))
+    parser.add_argument(
+        '-u', '--subset', type=str, default=None,
+        help='Subset of the labels to use for training (comma separated: ' + labels + ')',
+    )
     parser.add_argument('pretrained_model', help='Pretrained model to use for fine tuning',
                         choices=['mcbert', 'xlmrb', 'xlmrl'])
 
+    parser.add_argument('corpora', type=str, default=None,
+                        help='Corpora prefix or path prefix to use for training')
 
-def train_binrel(arg) -> 0:
-    logger.info("Starting train_binrel...")
+
+def train_binrel(arg) -> int:
+    logger.info('Starting binary relevance training ...')
+    labels = get_labels('nf', arg)
+    if not labels:
+        return 1
+
+    training_args = TrainingArguments(
+        output_dir=result_path,
+        num_train_epochs=arg.epochs,
+        per_device_train_batch_size=arg.batch,
+        per_device_eval_batch_size=arg.batch,
+        evaluation_strategy="epoch",
+        disable_tqdm=not arg.tqdm,
+        load_best_model_at_end=True,
+        save_strategy='epoch',
+        learning_rate=arg.learn_rate,
+        optim='adamw_torch',
+        # optim='adamw_hf',
+        save_total_limit=1,
+        metric_for_best_model='f1',
+        logging_strategy='epoch',
+    )
+
+    train_data, eval_data, test_data = DataSplit.load(get_data_path_prefix(arg))
+    for label in labels:
+        logger.info('Training for label: %s', label)
+        mc = SeqClassifyModel(
+            ModelContainer.model_name_map[arg.pretrained_model],
+            BinaryLabeler(labels=[label]),
+            os.path.join(arg.tmp_dir, arg.pretrained_model)
+        )
+        logger.debug("Constructing train data set [%s]...", len(train_data))
+        train_set = SeqClassifyDataset(
+            mc.labeler(), mc.tokenizer(), train_data, arg.max_seq_len, label_field, text_field
+        )
+        logger.info("Constructed train data set [%s].", len(train_data))
+        logger.debug("Constructing evaluation data set [%s]...", len(eval_data))
+        eval_set = SeqClassifyDataset(
+            mc.labeler(), mc.tokenizer(), eval_data, arg.max_seq_len, label_field, text_field
+        )
+        logger.info("Constructed evaluation data set [%s].", len(eval_data))
+        # train the model
+        mc.build(training_args, train_set, eval_set)
     return 0
 
 
-def train_lpset(arg) -> 0:
-    logger.info("Starting train_binrel...")
+def train_lpset(arg) -> int:
+    logger.info("Starting label powerset ...")
     return 0
---
+++ neuroticla/nf/utils.py
@@ -0,0 +1,41 @@
+import os
+import logging
+
+from argparse import ArgumentParser
+from typing import List, Dict
+
+from neuroticla.core.args import CommonArguments
+
+logger = logging.getLogger('nf.utils')
+
+
+def get_all_labels(ncra_module: str) -> List[str]:
+    labels_file = os.path.join(CommonArguments.data_path(ncra_module, 'processed'), 'tags.csv')
+    labels = open(labels_file, 'r').read().split('\n')
+    return labels
+
+
+def get_labels(nrcla_module: str, arg) -> List[str]:
+    labels = get_all_labels(nrcla_module)
+    if arg.subset is not None:
+        subset = arg.subset.split(',')
+        if all(item in labels for item in subset):
+            labels = subset
+        else:
+            logger.error('Invalid labels subset: %s', arg.subset)
+            return []
+    return labels
+
+
+def get_data_path_prefix(arg) -> List[str]:
+    data_paths = [os.path.join(arg.data_in_dir, arg.corpora)]
+    return data_paths
+
+
+def add_common_test_train_args(nrcla_module: str, parser: ArgumentParser) -> None:
+    CommonArguments.split_data_dir(nrcla_module, parser, ('-i', '--data_in_dir'))
+    CommonArguments.result_dir(nrcla_module, parser, ('-o', '--result_dir'))
+    CommonArguments.tmp_dir(nrcla_module, parser, ('-t', '--tmp_dir'))
+    parser.add_argument(
+        '--tqdm', help='Enable TDQM.', action='store_true', default=False
+    )
