7102ae7f18cea8900254877b0ddf84c8494dda4a
Nikola Ivačič <nikola.ivacic@dropchop.com>
2023-12-06 10:40:21 +0100

    Working on tests

--- neuroticla/play/corpus.py
+++ neuroticla/play/corpus.py
@@ -267,3 +267,61 @@ def corpus_dump(arg) -> int:
             logger.info("Dumped [%s::%s] for [%s]", prev_day, current_date, customer)
             current_date = prev_day
     return 0
+
+
+def corpus_correct(arg) -> int:
+    start_date = datetime.fromisoformat(arg.start_date)
+    end_date = datetime.fromisoformat(arg.end_date)
+    if os.path.exists(arg.customers):
+        with open(arg.customers) as f:
+            customers = f.read().splitlines()
+    else:
+        customers = arg.customers.split(',')
+    logger.info("Dumping [%s::%s] for %s", start_date, end_date, customers)
+    for customer in customers:
+        requests = Elastika()
+        requests.limit(9999)
+        requests.filter_customer(customer)
+        requests.field(['rubric', 'url'])
+
+        current_date = end_date
+        while current_date > start_date:
+            prev_day = current_date - timedelta(days=1)
+            day_dir = os.path.join(
+                arg.result_dir, str(current_date.year), f"{current_date.month:02d}", f"{current_date.day:02d}"
+            )
+            articles: List[Article] = requests.get(prev_day, current_date)
+            for a in articles:
+                article_file = os.path.join(day_dir, a.uuid + '.json')
+                if not os.path.exists(article_file):
+                    continue
+                with open(article_file, encoding='utf-8') as json_file:
+                    try:
+                        saved_article = json.load(json_file)
+                    except:
+                        logger.error("Unable to load json file [%s] for [%s].", article_file, a)
+                        os.remove(article_file)
+                        return 1
+
+                saved_article.pop('mediaReach', None)
+                url = a.data.pop('url', None)
+                if url:
+                    saved_article['url'] = url
+
+                saved_media = saved_article.get('media')
+                media = a.data.get('media')
+                if media and media.get('mediaReach', None):
+                    saved_media['mediaReach'] = media.get('mediaReach')
+
+                saved_rubric = saved_article.get('rubric')
+                rubric = a.data.get('rubric')
+                if rubric and rubric.get('mediaReach', None):
+                    saved_rubric['mediaReach'] = rubric.get('mediaReach')
+
+                with open(article_file, 'w', encoding='utf8') as json_file:
+                    json.dump(saved_article, json_file, indent='  ', ensure_ascii=False)
+
+                logger.info("Corrected [%s]", a)
+            logger.info("Corrected [%s::%s] for [%s]", prev_day, current_date, customer)
+            current_date = prev_day
+    return 0
