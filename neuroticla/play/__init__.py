2d5832bf2f0b8270162363a0d63dbc088a4ac4d6
Nikola Ivačič <nikola.ivacic@dropchop.com>
2024-09-23 15:29:45 +0200

    Added simple ner infer.

--- neuroticla/play/__init__.py
+++ neuroticla/play/__init__.py
@@ -7,6 +7,7 @@ MODULE_DESCRIPTOR = ModuleDescriptor(
     ModuleArguments([
         CommandArguments('cluster', 'Cluster compare', multi_action=True),
         CommandArguments('corpus', 'Corpus management', multi_action=True),
-        CommandArguments('sentiment', 'Sentiment corpus', multi_action=True)
+        CommandArguments('sentiment', 'Sentiment corpus', multi_action=True),
+        CommandArguments('ner', 'NER')
     ])
 )
